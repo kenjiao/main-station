9472938194e2dd3574f402077a1a00f413a8a492
lining <6732167702@qq.com>
2017-06-28 23:34:02 +0800

    打开融资网首页

---
+++ src/test/java/com/rongzi/ms/step_definitions/RongziSteps.java
@@ -0,0 +1,24 @@
+package com.rongzi.ms.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.zh_cn.假如;
+import cucumber.api.java.zh_cn.那么;
+
+/**
+ * Created by lining on 2017/6/28.
+ */
+public class RongziSteps extends StepDefs {
+
+    @假如("^我在主站首页$")
+    public void 我在主站首页() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @那么("^主站首页的主题是\"([^\"]*)\"$")
+    public void 主站首页的主题是(String title) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
