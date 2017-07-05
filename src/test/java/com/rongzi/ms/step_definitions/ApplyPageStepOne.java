bb13dad5aff1a41b1041898f73ab41c1d07ff0ec
liningning <673216770@qq.com>
2017-07-05 13:51:43 +0800

    测评页面

---
+++ src/test/java/com/rongzi/ms/step_definitions/ApplyPageStepOne.java
@@ -0,0 +1,14 @@
+package com.rongzi.ms.step_definitions;
+
+import cucumber.api.PendingException;
+
+/**
+ * Created by Administrator on 2017/7/5 0005.
+ */
+public class ApplyPageStepOne {
+    @cucumber.api.java.zh_CN.那么("^跳转到测评结果页面$")
+    public void 跳转到测评结果页面() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
