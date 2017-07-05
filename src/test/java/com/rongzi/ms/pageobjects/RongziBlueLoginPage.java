3dd4da749e7402c39a831966d6e827a606361661
Yuan Zhencai <zhencai.yuan@datarx.cn>
2017-07-05 21:51:39 +0800

    selects

--- src/test/java/com/rongzi/ms/pageobjects/RongziBlueLoginPage.java
+++ src/test/java/com/rongzi/ms/pageobjects/RongziBlueLoginPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.util.List;
+
 public class RongziBlueLoginPage extends BaseClass {
 
 	public RongziBlueLoginPage(WebDriver driver)
@@ -15,34 +17,23 @@ public class RongziBlueLoginPage extends BaseClass {
 	@FindBy(how=How.XPATH, using="//*[@name=\"userName\"]")
 	public static WebElement username;
 
-	@FindBy(how=How.XPATH, using="//*[@name=\"userIdentity\"]")
-	public static WebElement userIdentity;
-
-	@FindBy(how=How.CSS, using=".dropdown")
-	public static WebElement identitySelects;
+	@FindBy(how=How.CSS, using=".banner-content-right-infor .dropdown li")
+	public static List<WebElement> selects;
 
 	@FindBy(how=How.CSS, using=".genderBox")
 	public static WebElement gender;
 
+	@FindBy(how=How.XPATH, using="//*[@name=\"userIdentity\"]")
+	public static WebElement identity;
+
 	@FindBy(how=How.XPATH, using="//*[@name=\"loanMoney\"]")
 	public static WebElement loanMoney;
-
-	@FindBy(how=How.CSS, using=".dropdown .money")
-	public static WebElement loanMoneySelects;
 
 	@FindBy(how=How.XPATH, using="//*[@name=\"userMobile\"]")
 	public static WebElement userMobile;
 
 	@FindBy(how=How.CLASS_NAME, using="searchBtn freeBtn")
 	public static WebElement search_money;
-
-
-
-
-
-
-
-
 
 
 }
--- src/test/java/com/rongzi/ms/step_definitions/RongziBlueLoginSteps.java
+++ src/test/java/com/rongzi/ms/step_definitions/RongziBlueLoginSteps.java
@@ -1,10 +1,9 @@
 package com.rongzi.ms.step_definitions;
 
+import com.rongzi.ms.helpers.Log;
 import com.rongzi.ms.pageobjects.RongziBlueLoginPage;
-import cucumber.api.PendingException;
 import cucumber.api.java.zh_cn.同时;
 import cucumber.api.java.zh_cn.当;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
@@ -19,16 +18,34 @@ public class RongziBlueLoginSteps extends StepDefs {
     @当("^输入用户信息$")
     public void 输入用户信息() throws Throwable {
 
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Actions action = new Actions(driver);
+
+        PageFactory.initElements(driver, RongziBlueLoginPage.class);
+
+        RongziBlueLoginPage.username.sendKeys("lining");
+
+        List<WebElement> selects = RongziBlueLoginPage.selects;
+
+        action.moveToElement(RongziBlueLoginPage.gender).perform();
+        selects.get(1).click();
+        Log.info("选择女士");
+
+        action.moveToElement(RongziBlueLoginPage.identity).perform();
+        selects.get(3).click();
+        Log.info("选择企业主");
+
+        action.moveToElement(RongziBlueLoginPage.loanMoney).perform();
+        selects.get(7).click();
+        Log.info("选择3");
+
+        RongziBlueLoginPage.userMobile.sendKeys("18321950423");
 
     }
 
     @同时("^点击智能找贷款$")
     public void 点击智能找贷款() throws Throwable {
 
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        RongziBlueLoginPage.search_money.click();
 
     }
 }
