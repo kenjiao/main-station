390eadd8ed2973c2201d5afbeb66c2465b4ca6f5
lining <6732167702@qq.com>
2017-07-08 10:16:49 +0800

    title

--- src/test/java/com/rongzi/ms/modules/RongziAddressProceed.java
+++ src/test/java/com/rongzi/ms/modules/RongziAddressProceed.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class RongziAddressProceed {
 
     public static void execute(WebDriver driver, String title) {
 
-        assertEquals(driver.getTitle(), title);
+        assertTrue(driver.getTitle().contains(title));
         Reporter.log("Rongzi page verify successful");
     }
 
