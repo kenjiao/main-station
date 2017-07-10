41df3f7cf6cdbdeb5017000821e7637d7fc96ade
lining <6732167702@qq.com>
2017-07-10 23:13:41 +0800

    the bottom from index

---
+++ src/test/java/com/rongzi/ms/step_definitions/RecommendLoanMoneySteps.java
@@ -0,0 +1,45 @@
+package com.rongzi.ms.step_definitions;
+
+import cucumber.api.PendingException;
+
+/**
+ * Created by lining on 2017/7/10.
+ */
+public class RecommendLoanMoneySteps extends StepDefs{
+
+    @cucumber.api.java.zh_cn.当("^我点击我要申请$")
+    public void 我点击我要申请() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.那么("^跳转到智能贷，为您精准推荐优质贷款页面$")
+    public void 跳转到智能贷为您精准推荐优质贷款页面() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.同时("^填写联系方式,输入验证码$")
+    public void 填写联系方式输入验证码() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.并且("^输入验证码$")
+    public void 输入验证码() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.并且("^点击下一步$")
+    public void 点击下一步() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.当("^当我点击智能推荐处的免费体验$")
+    public void 当我点击智能推荐处的免费体验() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
