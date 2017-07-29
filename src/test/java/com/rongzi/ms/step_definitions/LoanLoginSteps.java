ac5d921096f599c831610e2d33f1f052df333e57
lining <6732167702@qq.com>
2017-07-29 23:51:30 +0800

    proxy enable

---
+++ src/test/java/com/rongzi/ms/step_definitions/LoanLoginSteps.java
@@ -0,0 +1,17 @@
+package com.rongzi.ms.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.zh_cn.假如;
+
+/**
+ * Created by lining on 2017/7/29.
+ */
+
+
+public class LoanLoginSteps {
+    @假如("^我在贷款着陆页首页$")
+    public void 我在贷款着陆页首页() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
