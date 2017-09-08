17c644211af668509ba71f1b0bf18c464c1ae0a0
李宁宁 <lining@liningningdeMacBook-Pro.local>
2017-09-08 17:18:40 +0800

    android

--- src/test/java/com/rongzi/ms/helpers/driver/AndroidBuilder.java
+++ src/test/java/com/rongzi/ms/helpers/driver/AndroidBuilder.java
@@ -1,6 +1,7 @@
 package com.rongzi.ms.helpers.driver;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -19,6 +20,17 @@ public class AndroidBuilder extends MobileDriverBuilder {
     @Override
     public WebDriver getWebDriver(URL remote) {
         return new AndroidDriver<>(remote, getCapabilities());
+    }
+
+    @Override
+    public DesiredCapabilities getCapabilities() {
+        DesiredCapabilities capabilities = super.getCapabilities();
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, false); //不初始化浏览器
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");  //设备名称
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Browser");
+        capabilities.setCapability("appPackage", "com.android.browser");
+        capabilities.setCapability("appActivity", ".BrowserActivity");
+        return capabilities;
     }
 
     public static class AndroidMeta extends MobileDriverMeta {
