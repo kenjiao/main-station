e381721cbaa71d80bc7f637205cf4869be1755a3
liningning <673216770@qq.com>
2017-09-08 09:24:55 +0800

    修复pc列表页和详情页元素找不到的问题

--- src/test/java/com/rongzi/ms/pageobjects/web/ProductDetailsPage.java
+++ src/test/java/com/rongzi/ms/pageobjects/web/ProductDetailsPage.java
@@ -14,7 +14,7 @@ public class ProductDetailsPage extends BaseClass {
         super(driver);
     }
 
-    @FindBy(how = How.CSS, using = ".right-formLoan > form > div.inforBox.nameBox > input")
+    @FindBy(how = How.XPATH, using = "/html/body/section[1]/div/div[2]/div[2]/form/div[1]/input")
     public static WebElement username;
 
     @FindBy(how = How.CSS, using = ".right-formLoan > form > div:nth-child(4) > input")
