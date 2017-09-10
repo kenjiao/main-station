639513ae380b6706027214ad894738efc2edfaba
李宁宁 <lining@liningningdeMBP.lan>
2017-09-10 11:29:39 +0800

    select city

---
+++ src/test/java/com/rongzi/ms/pageobjects/mobile/MCityPage.java
@@ -0,0 +1,15 @@
+package com.rongzi.ms.pageobjects.mobile;
+
+import com.rongzi.ms.pageobjects.BaseClass;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by lining on 2017/9/10.
+ */
+public class MCityPage extends BaseClass {
+
+    public MCityPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
--- src/test/java/com/rongzi/ms/step_definitions/mobile/MLoanLoginSteps.java
+++ src/test/java/com/rongzi/ms/step_definitions/mobile/MLoanLoginSteps.java
@@ -2,29 +2,50 @@ package com.rongzi.ms.step_definitions.mobile;
 
 import com.rongzi.ms.helpers.Env;
 import com.rongzi.ms.modules.LpLoginPageProceed;
-import com.rongzi.ms.modules.RongziAddressProceed;
 import com.rongzi.ms.pageobjects.mobile.MLoanLoginPage;
 import com.rongzi.ms.step_definitions.StepDefs;
-import cucumber.api.PendingException;
 import cucumber.api.java.zh_cn.假如;
 import cucumber.api.java.zh_cn.同时;
 import cucumber.api.java.zh_cn.并且;
 import cucumber.api.java.zh_cn.那么;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class MLoanLoginSteps extends StepDefs {
 
     @假如("^我在\"([^\"]*)\"着陆页$")
     public void 我在着陆页(String suffix) throws Throwable {
         String index = StringUtils.replace(Env.getProperty("rongzi.index"), "www", "m");
-        RongziAddressProceed.open(driver, index, Env.getProperty("rongzi.city", "上海市"));
+        String city = Env.getProperty("rongzi.city", "上海");
         LpLoginPageProceed.open(driver, index, suffix);
+
+        WebElement selectCity = driver.findElement(By.id("currentCity"));
+
+        selectCity.click();
+
+        List<WebElement> cities = driver.findElements(By.xpath("//*[@class=\"hot-city-section\"]/div/div/a"));
+
+        for (WebElement element : cities) {
+            if (element.getText().equals(city)) {
+                element.click();
+                return;
+            }
+
+        }
+
     }
 
     @并且("^输入贷款信息$")
     public void 输入贷款信息() throws Throwable {
         PageFactory.initElements(driver, MLoanLoginPage.class);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(MLoanLoginPage.username));
         MLoanLoginPage.username.sendKeys("li");
         MLoanLoginPage.mobile.sendKeys("18321950423");
         MLoanLoginPage.imgCode.sendKeys("1234");
@@ -38,7 +59,6 @@ public class MLoanLoginSteps extends StepDefs {
 
     @那么("^成功进入M站测评页面$")
     public void 成功进入m站测评页面() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        // TODO: 2017/9/10 assert
     }
 }
