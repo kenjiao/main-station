2e061444e03b16200f323837b815c7f50651d957
lining <673216770@qq.com>
2017-09-12 16:40:48 +0800

    start appium service

--- src/test/java/com/rongzi/ms/helpers/driver/MobileDriverBuilder.java
+++ src/test/java/com/rongzi/ms/helpers/driver/MobileDriverBuilder.java
@@ -3,15 +3,8 @@ package com.rongzi.ms.helpers.driver;
 import com.rongzi.ms.helpers.Env;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by lining on 2017/9/8.
@@ -22,26 +15,14 @@ public abstract class MobileDriverBuilder implements DriverBuilder, Proxy {
 
     private AppiumServiceBuilder builder;
 
-    private static Map<String, String> appiumMapping = new HashMap<>();
-
-    static {
-
-        appiumMapping.put(Platform.MAC.name(), "/usr/local/lib/node_modules/appium/build/lib/main.js");
-        appiumMapping.put(Platform.LINUX.name(), "/usr/local/lib/node_modules/appium/build/lib/main.js");
-        appiumMapping.put(Platform.WINDOWS.name(), "C:/Program Files (x86)/Appium/node_modules/appium/bin/appium.js");
-
-    }
-
 
     public MobileDriverBuilder(DesiredCapabilities capabilities) {
         this.capabilities = capabilities;
     }
 
     private void init() {
-
         builder = new AppiumServiceBuilder().
-                withAppiumJS(new File(appiumMapping.get(Platform.getCurrent().name()))).
-                withArgument(GeneralServerFlag.LOG_LEVEL, "info").
+                withArgument(GeneralServerFlag.LOG_LEVEL, Env.getProperty("appium.log.level", "info")).
                 usingAnyFreePort() /*and so on*/;
     }
 
@@ -50,6 +31,7 @@ public abstract class MobileDriverBuilder implements DriverBuilder, Proxy {
         init();
         return getWebDriver();
     }
+
 
     public void proxy() {
 
