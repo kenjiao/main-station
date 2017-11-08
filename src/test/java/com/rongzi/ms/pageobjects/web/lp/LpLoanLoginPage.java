d124054f0e58cb4157db5530b390b1179530c1b3
lining <673216770@qq.com>
2017-11-08 21:51:44 +0800

    LpLoanLoginPage.java

--- src/test/java/com/rongzi/ms/pageobjects/web/lp/LpLoanLoginPage.java
+++ src/test/java/com/rongzi/ms/pageobjects/web/lp/LpLoanLoginPage.java
@@ -35,7 +35,7 @@ public class LpLoanLoginPage extends BaseClass {
 			super(driver);
 		}
 
-		@FindBy(how=How.XPATH, using="//*[@id=\"userName2\"]")
+		@FindBy(how=How.XPATH, using="//*[@id=\"userName\"]")
 		public static WebElement username;
 
 		@FindBy(how=How.XPATH, using="//*[@id=\"userMobile2\"]")
