31ea6fdad0fcafdeb34b00cf8ec0c996940dd7a9
liningning <673216770@qq.com>
2017-12-27 19:33:18 +0800

    admin

---
+++ src/test/java/com/rongzi/ms/step_definitions/web/AdminLoginSteps.java
@@ -0,0 +1,26 @@
+package com.rongzi.ms.step_definitions.web;
+
+import cucumber.api.PendingException;
+
+/**
+ * Created by Administrator on 2017/12/27 0027.
+ */
+public class AdminLoginSteps {
+    @cucumber.api.java.zh_cn.假如("^我admin首页\"([^\"]*)\"$")
+    public void 我admin首页(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.并且("^输入登录信息$")
+    public void 输入登录信息() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.那么("^登录成功$")
+    public void 登录成功() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/resources/features/Admin.feature
@@ -0,0 +1,14 @@
+# language: zh-CN
+@admin
+功能: 登陆admin系统
+
+
+  场景: 登录admin
+    假如 我admin首页"http://admin.rongzi.com"
+    并且 输入登录信息
+    那么 登录成功
+
+
+
+
+
