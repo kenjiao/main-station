c7f95ecb0e7c1967d755ee6c5d2c3bbc3877d71d
liningning <673216770@qq.com>
2018-01-04 20:43:35 +0800

    CnLogin

--- src/test/java/com/rongzi/ms/pageobjects/web/CnLoginPage.java
+++ src/test/java/com/rongzi/ms/pageobjects/web/CnLoginPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.util.List;
+
 public class CnLoginPage extends BaseClass {
 
 	public CnLoginPage(WebDriver driver)
@@ -22,6 +24,36 @@ public class CnLoginPage extends BaseClass {
 	@FindBy(how=How.XPATH, using="//*[@id=\"btn_zhencerongzi1\"]")
 	public static WebElement immediate_consultation;
 
+    @FindBy(how=How.XPATH, using="/html/body/section[3]/div[1]/div/form/input")
+    public static WebElement obtain_return;
+
+    @FindBy(how=How.XPATH, using="//*[@id=\"divLoan\"]/form/input")
+    public static WebElement next_button;
+
+    public static class NextPage extends BaseClass {
+
+        public NextPage(WebDriver driver) {
+            super(driver);
+        }
+
+        @FindBy(how=How.XPATH, using="//*[@id=\"popName\"]")
+        public static WebElement username;
+
+        @FindBy(how=How.XPATH, using="//*[@id=\"popMobile\"]")
+        public static WebElement mobile;
+
+        @FindBy(how=How.XPATH, using="//*[@id=\"popCaptcha\"]")
+        public static WebElement graphic_verification_code;
+
+        @FindBy(how=How.XPATH, using="//*[@id=\"popSmscode\"]")
+        public static WebElement smscode;
+
+        @FindBy(how=How.XPATH, using="//*[@id=\"popSubmit\"]")
+        public static WebElement next_button;
+
+
+
+    }
 
 
 
--- src/test/java/com/rongzi/ms/step_definitions/web/lp/CnLoginSteps.java
+++ src/test/java/com/rongzi/ms/step_definitions/web/lp/CnLoginSteps.java
@@ -2,6 +2,7 @@ package com.rongzi.ms.step_definitions.web.lp;
 
 import com.rongzi.ms.pageobjects.web.CnLoginPage;
 import com.rongzi.ms.pageobjects.web.DialogCityPage;
+import com.rongzi.ms.pageobjects.web.ImmediateLoanPage;
 import com.rongzi.ms.step_definitions.StepDefs;
 import cucumber.api.PendingException;
 import org.openqa.selenium.support.PageFactory;
@@ -30,4 +31,24 @@ public class CnLoginSteps extends StepDefs{
     }
 
 
+    @cucumber.api.java.zh_cn.当("^点击获取每月最低还款$")
+    public void 点击获取每月最低还款() throws Throwable {
+        CnLoginPage.obtain_return.click();
+    }
+
+    @cucumber.api.java.zh_cn.并且("^填写下一步的信息$")
+    public void 填写下一步的信息() throws Throwable {
+       PageFactory.initElements(driver, CnLoginPage.NextPage.class);
+        CnLoginPage.NextPage.username.sendKeys("li");
+        CnLoginPage.NextPage.mobile.sendKeys("18321950423");
+        CnLoginPage.NextPage.graphic_verification_code.sendKeys("1234");
+        CnLoginPage.NextPage.smscode.sendKeys("123456");
+        CnLoginPage.NextPage.next_button.click();
+    }
+
+    @cucumber.api.java.zh_cn.同时("^获得最低贷款额$")
+    public void 获得最低贷款额() throws Throwable {
+       PageFactory.initElements(driver, ImmediateLoanPage.class);
+       ImmediateLoanPage.immediate_loan.click();
+    }
 }
