9ac46e5b1e986a9432e30280845817488aef884c
lining <673216770@qq.com>
2018-01-19 11:05:48 +0800

    MrecommendLoanMoneySteps MrecommendLoanMoneyPage MobileRongzi.feature

--- src/test/java/com/rongzi/ms/pageobjects/mobile/MRecommendLoanMoneyPage.java
+++ src/test/java/com/rongzi/ms/pageobjects/mobile/MRecommendLoanMoneyPage.java
@@ -18,17 +18,24 @@ public class MRecommendLoanMoneyPage extends BaseClass {
 	@FindBy(how = How.XPATH, using = "//*[@id=\"LoanPerod_scroll\"]/div/div[2]/div[2]/input")
 	public static WebElement loan_period;
 
+	@FindBy(how = How.XPATH, using = "//*[@id=\"Identity_scroll\"]/div[2]/div[2]/ul/li[1]/a")
+	public static WebElement identity;
+
+	@FindBy(how = How.XPATH, using = "//*[@id=\"CanProvideEarningsInformation_scroll\"]/div[2]/div[2]/ul/li[1]/a")
+	public static WebElement income_Prove;
+
+
 	@FindBy(how = How.XPATH, using = "//*[@id=\"WorkingCity_scroll\"]/div[2]/div[2]")
 	public static WebElement working_city;
-
-	@FindBy(how = How.XPATH, using = "//*[@id=\"SHANGHAI\"]")
-	public static WebElement city;
 
 	@FindBy(how = How.XPATH, using = "//*[@id=\"WorkingAge_scroll\"]/div[2]/div[2]")
 	public static WebElement working_age;
 
-	@FindBy(how = How.XPATH, using = "/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/ul/li[3]")
-	public static WebElement one_year;
+	@FindBy(how = How.XPATH, using = "//*[@id=\"Age_scroll\"]/div[2]/div[2]/input")
+	public static WebElement age;
+
+	@FindBy(how = How.XPATH, using = "//*[@id=\"Age_scroll\"]/div[2]/div[2]/input")
+	public static WebElement residence_time;
 
 	@FindBy(how = How.XPATH, using = "//*[@id=\"CensusRegisterSeatCity_scroll\"]/div[2]/div[2]")
 	public static WebElement register_city;
@@ -36,8 +43,6 @@ public class MRecommendLoanMoneyPage extends BaseClass {
 	@FindBy(how = How.XPATH, using = "//*[@id=\"CreditStanding_scroll\"]/div[2]/div[2]")
 	public static WebElement credit_standing;
 
-	@FindBy(how = How.XPATH, using = "/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/ul/li[1]")
-	public static WebElement credit_unknown;
 
 	@FindBy(how = How.XPATH, using = "//*[@id=\"UseLoanCity_scroll\"]/div[2]")
 	public static WebElement loan_city;
